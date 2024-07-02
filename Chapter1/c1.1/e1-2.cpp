61a1685f5e66336a5689848a01a338f1f172fc2c
JJtheron <theronjanjacques@gmail.com>
2024-07-01 20:03:43 -0700

    first commit

---
+++ Chapter1/c1.1/e1-2.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+main()
+{
+//  printf("1\n2\t3\c4\b5\q6\w7\e8\r9t\y0\y9\u00C0 8\i7\a6\s5\d-\f4\g-3\h-2\j-1\k-2\l-\z3\x452ff--\4-\5-\6-\7-\8-\9-\0,\0,9,8,7,6,5,4,\3,\2,\1end");
+//  printf("1\n2\t3\c4");
+  printf("\e8"); // go to top of window --interesting
+  printf("\a"); // allert
+/*
+\a  Alert or bell: Produces an audible alert or bell sound.
+\b  Backspace: Moves the cursor back by one position.
+\f  Form feed: Advances the printer to the next logical page.
+\n  Newline: Moves the cursor to the beginning of the next line.
+\r  Carriage return: Moves the cursor to the beginning of the current line.
+\t  Horizontal tab: Moves the cursor to the next horizontal tab stop.
+\v  Vertical tab: Moves the cursor to the next vertical tab stop.
+\\  Backslash: Inserts a backslash character.
+\'  Single quote: Inserts a single quote character.
+\”  Double quote: Inserts a double quote character.
+\?  Question mark: Inserts a question mark character.
+\0  Null character: Inserts a null character.
+\nnn  Octal value: Inserts the character represented by the octal value nnn.
+\xhh  Hexadecimal value: Inserts the character represented by the hexadecimal value hh.
+\uhhhh  Universal character name: Inserts the character represented by the Unicode value hhhh.*/
+}
